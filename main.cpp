e70a495be2de001240af63f300029c38adae7832
tdang93 <aznnoodle101@aol.com>
2017-05-11 15:55:15 -0700

    some changes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "Consumer.h"
 #include "Producer.h"
 
+#include <QTextEdit>
+#include <QHBoxLayout>
+
 int main(int argc, char **argv)
 {
     QList<int> myQList;
 
     Producer producer;
     producer.myQList_ptr = &myQList;
+
+    producer.fill();
 
     Consumer consumer;
     consumer.myQList_ptr = &myQList;
