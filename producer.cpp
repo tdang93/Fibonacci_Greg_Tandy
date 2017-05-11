d1d7a2817d7475d2751fb81b5e8973f4151cdcee
Unknown <omegazeron@yahoo.com>
2017-05-11 15:57:24 -0700

    Started Producer branch

--- producer.cpp
+++ producer.cpp
@@ -12,5 +12,25 @@ Producer::~Producer()
 
 void Producer::fill(int times)
 {
+    int f = 0;
+    int s = 1;
 
+
+    //number of outputs
+    for (int i = 0; i < times; ++i)
+    {
+        if (i = 0)
+        {
+            //cout 0
+        }
+        else if (i = 1)
+        {
+            //cout << " 1"
+            break;
+        }
+
+        else
+        //calculation
+        //cout << s
+    }
 }
