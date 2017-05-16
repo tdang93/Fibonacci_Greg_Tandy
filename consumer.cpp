ed88f56dc552ef43519e10f2fc790297fcd67d53
tdang93 <aznnoodle101@aol.com>
2017-05-16 14:29:24 -0700

    formatted output

--- consumer.cpp
+++ consumer.cpp
@@ -20,10 +20,14 @@ void Consumer::pour()
     QMutex_ptr->lock(); // lock the thread
 
     QString QS;
+    int counter = 0;
     while(!QList_ptr->isEmpty())
     {
+        counter++;
+        QS += "#";
+        QS += QString::number(counter) += ": ";
         QS += QString::number(QList_ptr->takeFirst());
-        QS += " ";
+        QS += "\n" ;
     }
     QMutex_ptr->unlock(); // unlock the thread
 
